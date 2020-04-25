630636bdbe79053a16281ba95bd239a876b6a67c
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2020-04-25 09:48:04 +0200

    set port

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 source /opt/envs/klamu/bin/activate
 # flask run &
-exec gunicorn -b :8005 --access-logfile - --error-logfile - fromflask:app &
+exec gunicorn -b :8006 --access-logfile - --error-logfile - fromflask:app &
